eeba19a497722af552d92bd0053ec2eaef8f0fa3
Alec Trievel <atrievel@ceiamerica.com>
2020-05-24 04:50:22 -0400

    Add routing

--- wwwroot/src/app/app-routing.module.ts
+++ wwwroot/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainDisplayComponent } from './main-display/main-display.component';
+import { ProjectsDisplayComponent } from './projects-display/projects-display.component';
+import { BlogsDisplayComponent } from './blogs-display/blogs-display.component';
+import { ResumeDisplayComponent } from './resume-display/resume-display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: MainDisplayComponent },
+  { path: 'projects', component: ProjectsDisplayComponent },
+  { path: 'blogs', component: BlogsDisplayComponent },
+  { path: 'resume', component: ResumeDisplayComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
